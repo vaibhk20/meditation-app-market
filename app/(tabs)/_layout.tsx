682fedb9436def954eed27f8a206d9440b4d7f31
Sanchitv3 <sanchitverma03032003@gmail.com>
2024-11-26 15:29:22 +0530

    added shift animation in tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -35,6 +35,7 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
+        animation: "shift",
       }}
       tabBar={(props) => <CustomTabBar tabItems={tabItems} />}
     >
